96c22d9d0c61363f8d3b277c868182557cf75717
David Saeva <dsaeva@localhost.localdomain>
2018-03-17 20:43:26 -0400

    db init file updated

--- src/main/java/com/revature/reQUEST/pojo/Request.java
+++ src/main/java/com/revature/reQUEST/pojo/Request.java
@@ -4,7 +4,13 @@ public class Request {
 	
 	private int id;
 	private int requestorId;
-	
-	
+	private int sprvsrId;
+	private int sprvsrApproval;
+	private int deptHeadId;
+	private int deptHeadApproval;
+	private String eventName;
+	private double eventCost;
+	private String eventType;
+	private double coverageAmt;
 	
 }
