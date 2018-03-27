e3d5e62b086f0750c11d3aa3db24801ac5406913
David Saeva <dsaeva@localhost.localdomain>
2018-03-27 19:21:38 -0400

    fixes for tomcat deploy, mostly working authservlet

--- src/main/java/com/revature/reQUEST/App.java
+++ src/main/java/com/revature/reQUEST/App.java
@@ -1,5 +1,14 @@
 package com.revature.reQUEST;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import com.revature.reQUEST.pojo.Employee;
+import com.revature.reQUEST.pojo.LoginCredentials;
+import com.revature.reQUEST.util.EmployeeUtils;
+import com.revature.reQUEST.util.JwtUtil;
+import com.revature.reQUEST.util.TokenBox;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	/*LoginCredentials user = new LoginCredentials("j.stevens@fake.com", "1234");
+    	EmployeeUtils.setEmp(user);
+        System.out.println(EmployeeUtils.authenticate());
+        
+        Employee emp = EmployeeUtils.getEmp();
+		
+		JwtUtil tokenService = new JwtUtil();
+		try {
+			TokenBox.setSessionToken(tokenService.generateToken(emp));
+		} catch (NoSuchAlgorithmException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		// TODO store token and send response
+		if (TokenBox.getSessionToken() != null) {
+			System.out.println("Authorization: "+TokenBox.getSessionToken());
+		}*/
     }
 }
--- src/main/java/com/revature/reQUEST/servlet/AuthServlet.java
+++ src/main/java/com/revature/reQUEST/servlet/AuthServlet.java
@@ -1,21 +1,24 @@
 package com.revature.reQUEST.servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.reQUEST.pojo.Employee;
 import com.revature.reQUEST.pojo.LoginCredentials;
 import com.revature.reQUEST.util.EmployeeUtils;
 import com.revature.reQUEST.util.JwtUtil;
 import com.revature.reQUEST.util.TokenBox;
 
-@WebServlet(name="AuthServlet", urlPatterns= {"/auth"})
+
 public class AuthServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -23,12 +26,13 @@ public class AuthServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		// retrieve the email and password from the request
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
+		String userParams = getBody(request);
+		ObjectMapper mapper = new ObjectMapper();
+		LoginCredentials user = mapper.readValue(userParams, LoginCredentials.class);
+		
 		
 		// authenticate user
 		Employee emp;
-		LoginCredentials user = new LoginCredentials(email, password);
 		EmployeeUtils.setEmp(user);
 		boolean authenticated = EmployeeUtils.authenticate();
 		
@@ -45,12 +49,46 @@ public class AuthServlet extends HttpServlet {
 			// TODO store token and send response
 			if (TokenBox.getSessionToken() != null) {
 				response.addHeader("Authorization", TokenBox.getSessionToken());
-				response.sendRedirect("landing.html");
+				response.sendRedirect("/landing");
 			}
 			
 		} else {
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			return;
+			request.getRequestDispatcher("/login").forward(request, response);
+
 		}
+	}
+
+	public static String getBody(HttpServletRequest request) throws IOException {
+
+	    String body = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+
+	    try {
+	        InputStream inputStream = request.getInputStream();
+	        if (inputStream != null) {
+	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	            char[] charBuffer = new char[128];
+	            int bytesRead = -1;
+	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+	                stringBuilder.append(charBuffer, 0, bytesRead);
+	            }
+	        } else {
+	            stringBuilder.append("");
+	        }
+	    } catch (IOException ex) {
+	        throw ex;
+	    } finally {
+	        if (bufferedReader != null) {
+	            try {
+	                bufferedReader.close();
+	            } catch (IOException ex) {
+	                throw ex;
+	            }
+	        }
+	    }
+
+	    body = stringBuilder.toString();
+	    return body;
 	}
 }
